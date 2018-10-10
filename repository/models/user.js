864448caa28331c2b0c6600a637d1cc8665b3521
keyne.kassapa <k.oei@uqconnect.edu.au>
2018-10-11 06:13:18 +1000

    add users model

---
+++ repository/models/user.js
@@ -0,0 +1,62 @@
+/*
+The code snippet below has been adapted from
+
+Node JS Configuration
+https://www.youtube.com/watch?v=uONz0lEWft0&list=PLillGF-RfqbZMNtaOXJQiDebNXjVapWPZ
+
+I have used their reference to make my own function. The function here is connected to the node js server.
+*/
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type:String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {strict: false});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+  const query = {username: username};
+  User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if(err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
+
+module.exports.comparePassword = function(password, hash, callback){
+  bcrypt.compare(password, hash, (err, isMatch) => {
+    if (err) throw err;
+    callback(null, isMatch);
+  });
+}
+
+module.exports.removeUserByUsername = function(d_username, callback){
+  User.deleteOne({username: d_username}, callback);
+}
