a62a3bcee6e5b3b4daf13f4e5fecc0ddb5af57a6
keyne.kassapa <k.oei@uqconnect.edu.au>
2018-10-15 01:39:19 +1000

    Update the register interface

---
+++ repository/public/assets/js/index.js
@@ -0,0 +1,37 @@
+$('document').ready(function(){
+  if ($('.container').hasClass('login')){
+    $('#login-menu').addClass('active');
+
+  } else if ( $('.container').hasClass('register')) {
+    $('#signup-menu').addClass('active');
+
+    $.ajax({
+      url: 'http://localhost:3000/users/usersList',
+      type: 'GET',
+      dataType: 'json',
+      success: function(result){
+        console.log(result);
+
+        var options = {
+          valueNames: [ 'name', 'born' ],
+          item: '<li><h3 class="name"></h3><p class="born"></p></li>'
+        };
+
+        var values = [{
+          name: "Martina Elm",
+          born: 1986
+          }, {
+          name: "Martina Elm 2",
+          born: 1986
+          },
+        ];
+
+        var userList = new List('users', options, values);
+
+      }
+    })
+
+  } else if ( $('.container').hasClass('home')) {
+    $('#home-menu').addClass('active');
+  }
+});
