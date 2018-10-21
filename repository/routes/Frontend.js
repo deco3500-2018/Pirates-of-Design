8ce8c121547666f4e9062ee69b493d3dd061020c
keyne.kassapa <k.oei@uqconnect.edu.au>
2018-10-21 22:04:23 +1000

    Add category on detail

--- repository/routes/Frontend.js
+++ repository/routes/Frontend.js
@@ -21,19 +21,19 @@ router.get('/example', function (req, res) {
 
 // referral-detail
 router.get('/gp/referral/:id/backend', function (req, res) {
-  Referral.find({'_id': req.params.id}, ['name', 'patient_name', 'patient_address', 'patient_phonum', 'patient_dob', 'description', 'gp_id', 'state', 'physician_id', 'created_at'], function(err,Referrals){
+  Referral.find({'_id': req.params.id}, ['name', 'patient_name', 'patient_address', 'patient_phonum', 'patient_dob', 'description', 'gp_id', 'state', 'physician_id', 'created_at', 'category'], function(err,Referrals){
     res.send(Referrals);
   });
 })
 
 router.get('/physician/referral/:id/backend', function (req, res) {
-  Referral.find({'_id': req.params.id}, ['name', 'patient_name', 'patient_address', 'patient_phonum', 'patient_dob', 'description', 'gp_id', 'state', 'physician_id', 'created_at'], function(err,Referrals){
+  Referral.find({'_id': req.params.id}, ['name', 'patient_name', 'patient_address', 'patient_phonum', 'patient_dob', 'description', 'gp_id', 'state', 'physician_id', 'created_at', 'category'], function(err,Referrals){
     res.send(Referrals);
   });
 })
 
 router.get('/physician/referral-wait/:id/backend', function (req, res) {
-  Referral.find({'_id': req.params.id}, ['name', 'patient_name', 'patient_address', 'patient_phonum', 'patient_dob', 'description', 'gp_id', 'state', 'physician_id', 'created_at'], function(err,Referrals){
+  Referral.find({'_id': req.params.id}, ['name', 'patient_name', 'patient_address', 'patient_phonum', 'patient_dob', 'description', 'gp_id', 'state', 'physician_id', 'created_at', 'category'], function(err,Referrals){
     res.send(Referrals);
   });
 })
